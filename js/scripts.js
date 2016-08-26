79c8d5b1dcbd949d2efd833eae635b490673712f
Talayruk <notejde@gmail.com>
2016-08-26 15:43:00 -0700

    added #contacts.empty() to empty out the previous input

--- js/scripts.js
+++ js/scripts.js
@@ -22,13 +22,15 @@ $(document).ready(function() {
 
 
     $(".contact").last().click(function() {
+      $("#contacts").empty();
       $("#show-contact").show();
-      $("#show-contact h2").text(newContact.firstName);
+      $("#show-contact h2").text(newContact.fullName());
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
     });
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("")
+
   });
 });
