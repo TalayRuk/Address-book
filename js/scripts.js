fa37a23e8a9fd2a34c8379fa2bfdb1946e26f3dd
Talayruk <notejde@gmail.com>
2016-08-27 19:50:29 -0700

    added html for address and backend logic for address

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,46 @@
 function Contact(first,last) {
   this.firstName = first;
   this.lastName = last;
+  this.addresses = [];
+}
+function Address(street, city, state){
+  this.street = street;
+  this.city = city;
+  this.state = state
 }
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
-
+Address.prototype.street = function() {
+  return this.street + " " + this.city + " " + this.state
+}
 //user interface logic
+// Address $call back function
+// ***should not be in the form submit listerner callback function.
+// this is b/c the btn must be functional b4 we submit the form; thus,
+// the user have option to add more than one address to a Contact, they
+// can just click "another address btn to receive more address form fields
+// b4 submitting the form to create the new Contact!"
+$(document).ready(function() {
+  $("#add-address").click(funtion() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-street">Street</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-city">City</label>' +
+                                   '<input type="text" class="form-control new-city">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-state">State</label>' +
+                                   '<input type="text" class="form-control new-state">' +
+                                 '</div>' +
+                               '</div>');
+  });
+});
+
 $(document).ready(function() {
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
